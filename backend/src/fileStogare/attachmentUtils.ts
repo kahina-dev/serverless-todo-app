b2df7c72ff93d3676874d0735a1060852adcd21d
Kahina <ait_kahina@hotmail.com>
2021-09-30 18:08:58 +0100

    Add file storage

--- backend/src/fileStogare/attachmentUtils.ts
+++ backend/src/fileStogare/attachmentUtils.ts
@@ -7,8 +7,17 @@ export class FileStorage{
 
     constructor(
         private readonly s3 = new XAWS.S3({
-            signatureVersion: 'v4'
-          }),
+            signatureVersion: 'v4'}),
         private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
         private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION) { }
+
+        async getUploadUrl(todoId: string): Promise<string> {
+            console.log(`Generating upload URL`)
+            return this.s3.getSignedUrl('putObject', {
+              Bucket: this.bucketName,
+              Key: todoId,
+              Expires: this.urlExpiration
+            })
+            
+          }
 }
